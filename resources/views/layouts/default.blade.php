62a325b0faa68f43930ac76f8e5a6c29bec9939a
wang junyu <277272841@qq.com>
2020-08-08 12:00:16 +0000

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+    <title>@yield('title','this is default title')</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+    <h1>主页</h1>
+@stop
+
+@section('title')
+    home
+@stop
