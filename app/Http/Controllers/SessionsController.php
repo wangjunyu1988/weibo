13f7b6fe12eeb5036ccf3c17720eaf74726a7eda
wang junyu <277272841@qq.com>
2020-08-11 03:41:58 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,22 +8,29 @@ use Illuminate\Support\Facades\Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
     public function create(){
         return view('sessions.create');
     }
 
     public function store(Request $request){
-        $credentials = $this->validate($request,[
+        $credentials = $this->validate($request, [
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        if(Auth::attempt($credentials,$request->has('remember'))){
-            //login success
-            session()->flash('success','欢迎回来！');
-            return redirect()->route('users.show',[Auth::user()]);
-        }else{
-            //login fail
-            session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
+
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            session()->flash('success', '欢迎回来！');
+            $fallback = route('users.show', Auth::user());
+            return redirect()->intended($fallback);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
         return;
