e0f8b665edc6a6005092b0f8d434a1aaf413fc35
wang junyu <277272841@qq.com>
2020-08-12 02:42:48 +0000

    发布微博

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::post('login','SessionsController@store')->name('login'); //登录、创�
 Route::delete('logout','SessionsController@destroy')->name('logout');
 
 Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
+
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
